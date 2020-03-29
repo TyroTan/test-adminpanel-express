8de7d07bf92a3d3bc3837d64ddaad9d96bf62afa
Tyro <tyro@crowdbotics.com>
2020-03-30 00:59:39 +0800

    init lint, prettier, typescript init sequelize and postgres;

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
+import express from 'express';
+
+// eslint-disable-next-line import/first
+import { User } from './src/db/model';
+
 const app = express();
 
+User.sync();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Example app listening on port 8000!');
 });
